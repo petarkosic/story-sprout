97dee4a1b1c2a71368d9d268f80f630e8d6a6a56
petarkosic <avt3914@gmail.com>
2024-05-08 23:48:16 +0200

    add: display sentences
    Get sentences from the server and display them in the UI.

--- client/src/views/StoryView.tsx
+++ client/src/views/StoryView.tsx
@@ -1,13 +1,58 @@
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
+type Sentence = {
+	sentence_id: number;
+	story_id: number;
+	parent_sentence_id: number;
+	content: string;
+	created_at: Date;
+	story_headline: string;
+	rating: number;
+};
+
 function StoryView() {
+	const [sentences, setSentences] = useState<Sentence[] | undefined>([]);
+
 	const { state } = useLocation();
 
+	useEffect(() => {
+		async function fetcnSentences() {
+			fetch(`http://localhost:5000/api/v1/stories/${state.story.story_id}`)
+				.then((res) => res.json())
+				.then((sentences) => setSentences(sentences.sentences))
+				.catch((err) => console.error(err));
+		}
+
+		fetcnSentences();
+	}, [state.story.story_id]);
+
 	return (
-		<div>
-			<p>{state.story.story_headline}</p>
-			<p>Story ID: {state.story.story_id}</p>
-		</div>
+		<>
+			<div className='story-container'>
+				<div className='story-details'>
+					<h1>{state.story.story_headline}</h1>
+					<div className='story-info'>
+						<p>Story details</p>
+						<div className='story-stats'>
+							<p>Number of contributions: </p>
+							<p>Rating: {state.story.rating || 'Not rated yet'}</p>
+						</div>
+					</div>
+				</div>
+				<div className='story-actions'>
+					{sentences?.map((sentence) => (
+						<div key={sentence.sentence_id}>
+							<p key={sentence.sentence_id}>{sentence.content}</p>
+							<span>Sentence ID: {sentence.sentence_id}</span>
+							<span>
+								Parent Sentence ID: {sentence.parent_sentence_id || 'None'}
+							</span>
+						</div>
+					))}
+				</div>
+			</div>
+		</>
 	);
 }
 
