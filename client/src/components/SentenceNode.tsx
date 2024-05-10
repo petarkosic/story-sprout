3ee4978a0f354afb74b7c7eb356844108bf76657
petarkosic <avt3914@gmail.com>
2024-05-10 23:58:37 +0200

    add: modal to add new sentence
    Show the modal when the user clicks the add button.

--- client/src/components/SentenceNode.tsx
+++ client/src/components/SentenceNode.tsx
@@ -1,6 +1,28 @@
+import { useState } from 'react';
 import { type Sentence } from '../utils/types';
 
 const SentenceNode = ({ sentence }: { sentence: Sentence }) => {
+	const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+	const [inputValue, setInputValue] = useState('');
+
+	const handleCloseModal = () => {
+		setIsModalOpen(false);
+		setInputValue(''); // Reset input value when modal is closed
+	};
+
+	const handleSubmit = () => {
+		console.log('Submitted:', inputValue);
+		handleCloseModal();
+	};
+
+	const handleAddSentence = () => {
+		setIsModalOpen((prev) => !prev);
+	};
+
+	const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+		setInputValue(e.target.value);
+	};
+
 	return (
 		<div
 			className='sentence-wrapper'
@@ -11,13 +33,30 @@ const SentenceNode = ({ sentence }: { sentence: Sentence }) => {
 		>
 			<div className='sentence'>
 				<p>{sentence.content}</p>
-				<button className='button-add' onClick={() => console.log(sentence)}>
+				<button className='button-add' onClick={() => handleAddSentence()}>
 					+
 				</button>
 			</div>
 			{sentence.children?.map((child: Sentence) => (
 				<SentenceNode key={child.sentence_id} sentence={child} />
 			))}
+
+			{isModalOpen && (
+				<div className='modal'>
+					<div className='modal-content'>
+						<span className='close' onClick={() => handleCloseModal()}>
+							&times;
+						</span>
+						<textarea
+							maxLength={100}
+							value={inputValue}
+							onChange={(e) => handleInputChange(e)}
+							placeholder='Enter a sentence'
+						></textarea>
+						<button onClick={() => handleSubmit()}>Submit</button>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 };
