4c81a7ac2da856f35fae6fae4602e988a86e4a6f
petarkosic <avt3914@gmail.com>
2024-05-20 23:31:16 +0200

    add: login and register components
    Setup initial implementation of login and register components.

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,19 +1,53 @@
+import { useState } from 'react';
+
 type LoginProps = {
 	toggleView: () => void;
 };
 
 const Login = ({ toggleView }: LoginProps) => {
+	const [email, setEmail] = useState<string>('');
+	const [password, setPassword] = useState<string>('');
+
+	async function sendData() {
+		const response = await fetch('http://localhost:5000/api/v1/auth/login', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ email, password }),
+		});
+
+		const json = await response.json();
+		console.log({ json });
+	}
+
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+
+		sendData();
+	};
+
 	return (
 		<div className='auth-container'>
 			<h2>Login</h2>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<div>
 					<label>Email:</label>
-					<input type='email' name='email' />
+					<input
+						type='email'
+						name='email'
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
 				</div>
 				<div>
 					<label>Password:</label>
-					<input type='password' name='password' />
+					<input
+						type='password'
+						name='password'
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
 				</div>
 				<button type='submit'>Login</button>
 			</form>
--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -1,29 +1,80 @@
+import { useState } from 'react';
+
 type RegisterProps = {
 	toggleView: () => void;
 };
 
-// TODO: fix input[type='text'] styling for first name and last name
-
 const Register = ({ toggleView }: RegisterProps) => {
+	const [firstName, setFirstName] = useState<string>('');
+	const [lastName, setLastName] = useState<string>('');
+	const [email, setEmail] = useState<string>('');
+	const [password, setPassword] = useState<string>('');
+
+	async function sendData() {
+		const data = {
+			first_name: firstName,
+			last_name: lastName,
+			email: email,
+			password: password,
+		};
+
+		const response = await fetch('http://localhost:5000/api/v1/auth/register', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(data),
+		});
+
+		const json = await response.json();
+		console.log({ json });
+	}
+
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+
+		sendData();
+	};
+
 	return (
 		<div className='auth-container'>
 			<h2>Register</h2>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<div>
 					<label>First Name:</label>
-					<input type='text' name='first_name' />
+					<input
+						type='text'
+						name='first_name'
+						value={firstName}
+						onChange={(e) => setFirstName(e.target.value)}
+					/>
 				</div>
 				<div>
 					<label>Last Name:</label>
-					<input type='text' name='last_name' />
+					<input
+						type='text'
+						name='last_name'
+						value={lastName}
+						onChange={(e) => setLastName(e.target.value)}
+					/>
 				</div>
 				<div>
 					<label>Email:</label>
-					<input type='email' name='email' />
+					<input
+						type='email'
+						name='email'
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
 				</div>
 				<div>
 					<label>Password:</label>
-					<input type='password' name='password' />
+					<input
+						type='password'
+						name='password'
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
 				</div>
 				<button type='submit'>Register</button>
 			</form>
