44047c16d7b3579c02123e60b6387ab722ddb71a
petarkosic <avt3914@gmail.com>
2024-06-05 23:51:29 +0200

    add: addNewStory storiesSlice
    Implement addNewStory function in storiesSlice.
    Use addNewStory inside navbar.

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { clearState, logoutUser } from '../features/auth/authSlice';
 import { AppDispatch, RootState } from '../store';
+import { addNewStory } from '../features/stories/storiesSlice';
 
 const Navbar = () => {
+	const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+	const [inputValue, setInputValue] = useState<string>('');
 	const navigate = useNavigate();
 	const dispatch = useDispatch<AppDispatch>();
 	const user = useSelector((state: RootState) => state.auth.user);
@@ -18,6 +22,31 @@ const Navbar = () => {
 		navigate('/auth');
 	};
 
+	const handleCloseModal = () => {
+		setIsModalOpen(false);
+		setInputValue('');
+	};
+
+	const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+		setInputValue(e.target.value);
+	};
+
+	const handleSubmit = () => {
+		if (!user) {
+			navigate('/auth', { state: { error: 'Login to add a sentence' } });
+			return;
+		}
+
+		dispatch(
+			addNewStory({
+				story_headline: inputValue,
+				user_id: user.user_id,
+			})
+		);
+
+		handleCloseModal();
+	};
+
 	return (
 		<>
 			<div className='navbar-container'>
@@ -27,6 +56,34 @@ const Navbar = () => {
 						<div>
 							{user?.first_name} {user?.last_name[0] + '.'}
 						</div>
+						<div
+							className='add-story-button'
+							onClick={() => setIsModalOpen(true)}
+						>
+							+
+						</div>
+
+						{isModalOpen && (
+							<div className='modal'>
+								<div className='modal-content modal-content-add'>
+									<span className='close' onClick={() => handleCloseModal()}>
+										&times;
+									</span>
+									<form>
+										<textarea
+											name='story-headline'
+											value={inputValue}
+											onChange={(e) => handleInputChange(e)}
+											placeholder='Create new story'
+										></textarea>
+										<button type='submit' onClick={handleSubmit}>
+											Submit
+										</button>
+									</form>
+								</div>
+							</div>
+						)}
+
 						<button className='button-logout' onClick={handleLoguot}>
 							logout
 						</button>
--- client/src/features/stories/storiesSlice.ts
+++ client/src/features/stories/storiesSlice.ts
@@ -31,6 +31,38 @@ export const getStories = createAsyncThunk(
 	}
 );
 
+export const addNewStory = createAsyncThunk(
+	'stories/addNewStory',
+	async (
+		{ story_headline, user_id }: { story_headline: string; user_id: string },
+		{ rejectWithValue, dispatch }
+	) => {
+		try {
+			const response = await fetch('http://localhost:5000/api/v1/stories/new', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+				},
+				body: JSON.stringify({ story_headline, user_id }),
+				credentials: 'include',
+			});
+
+			const data = await response.json();
+
+			if (response.ok) {
+				dispatch(getStories());
+
+				return data.newStory;
+			} else {
+				return rejectWithValue(data.message);
+			}
+		} catch (error) {
+			return rejectWithValue(error);
+		}
+	}
+);
+
 const storiesSlice = createSlice({
 	name: 'stories',
 	initialState,
